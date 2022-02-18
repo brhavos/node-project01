4c29dbf0fdcdeb3c535c850ff26d32ea0f8ded88
brhavos <brhavos@gmail.com>
2022-02-18 10:07:08 -0300

    Criei a rota de visualização com passagem de parâmetro

--- app.js
+++ app.js
@@ -15,21 +15,48 @@ app.get('/contato', (req, res) => {
 });
 
 
-// Rota: Mostrando um contato específico usando um retorno de um objeto compleso
+// Rota: /contato2
+// Mostrando um contato específico usando um retorno de um objeto complexo
 // em json.
 // Acesso: http://localhost:8080/contato2
 app.get('/contato2', (req, res) => {
-  //res.send("Ver Contato");
+
   return res.json({
     "nome": "Carlos",
     "email": "brhavos@gmail.com.br" 
-  })
+  });
+
 });
 
 
 
+// Rota: /contato3
+// Mostrando um contato específico usando um retorno de um objeto complexo
+// em json e com passagem de um parâmetro.
+// No exemplo vamos supor que o "id" da pessoa é o "100".
+// Exemplos de acesso: 
+//  http://localhost:8080/contato3/100
+//  http://localhost:8080/contato3/14
+//  http://localhost:8080/contato3/5
+//  http://localhost:8080/contato3/1200
+//  etc.
+
+app.get("/contato3/:id", (req, res) => {
+
+  // Atribuindo o "id" fornecido como parâmetro numa constante.
+  const c_id = req.params.id;
+
+  return res.json({
+    id: c_id,
+    nome: "Carlos",
+    email: "brhavos@gmail.com.br" 
+  });
+
+});
 
 
+
+// --------------------------------------------------
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
