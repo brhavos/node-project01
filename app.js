2aa692d459b3b14b82a40161ac1bd71b38623e62
brhavos <brhavos@gmail.com>
2022-02-18 14:37:57 -0300

    Criei a rota com um parâmetro e duas variáveis

--- app.js
+++ app.js
@@ -31,9 +31,8 @@ app.get('/contato2', (req, res) => {
 
 
 // Rota: /contato3
-// Mostrando um contato específico usando um retorno de um objeto complexo
-// em json e com passagem de um parâmetro.
-// No exemplo vamos supor que o "id" da pessoa é o "100".
+// Mostrando um contato com passagem de um parâmetro.
+// O retorno será um objeto complexo em json.
 // Exemplos de acesso: 
 //  http://localhost:8080/contato3/100
 //  http://localhost:8080/contato3/14
@@ -50,6 +49,38 @@ app.get("/contato3/:id", (req, res) => {
     id: c_id,
     nome: "Carlos",
     email: "brhavos@gmail.com.br" 
+  });
+
+});
+
+
+
+
+
+// Rota: /contato4
+// Executando a API enviando 2 dados:
+//  id
+//  situação
+// O retorno será um objeto complexo em json mostrado pelo GET.
+// Exemplos de uso: 
+//  http://localhost:8080/contato4/100?situacao=ativo&pessoa=juridica
+//  http://localhost:8080/contato4/200?situacao=inativo&pessoa=fisica
+//  etc.
+
+app.get("/contato4/:id", (req, res) => {
+
+  // Pegando o "id", via params...
+  const c_id = req.params.id;
+  // Pegando a "situação", via query...
+  const c_situacao = req.query.situacao;
+  const c_pessoa = req.query.pessoa;
+
+  return res.json({
+    id: c_id,
+    nome: "Carlos",
+    email: "brhavos@gmail.com.br",
+    situacao: c_situacao,
+    pessoa: c_pessoa
   });
 
 });
