d62fe99cd2b4d6a5d8ed9b7b64276055b39a0062
brhavos <brhavos@gmail.com>
2022-02-18 09:45:43 -0300

    Criei 2 rotas - uma para ver contato e outra para mostrar um contato específico

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const express = require('express');
 const app = express();
 
 // get é para ver uma página no navegador...
-// Rota:
-// Nome: Listar
+// Rota: Listar (nome dado no Insomnia)
 // Acesso: http://localhost:8080
 app.get('/', (req, res) => {
     res.send("Carlos, seja muito bem vindo!");
 });
+
+// Rota: Mostra a página do contato.
+// Acesso: http://localhost:8080/contato
+app.get('/contato', (req, res) => {
+  res.send("Ver Contato");
+});
+
+
+// Rota: Mostrando um contato específico usando um retorno de um objeto compleso
+// em json.
+// Acesso: http://localhost:8080/contato2
+app.get('/contato2', (req, res) => {
+  //res.send("Ver Contato");
+  return res.json({
+    "nome": "Carlos",
+    "email": "brhavos@gmail.com.br" 
+  })
+});
+
+
+
 
 
 
